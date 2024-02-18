07ac488d66b3bb69648503cf595e19b10f16d922
Klagarge <remi@heredero.ch>
2024-02-18 11:06:32 +0100

    try reverse rotor

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@
 char ROMAN[26]                  = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 char STATIC_ROTOR[26]           = {'T', 'L', 'B', 'V', 'R', 'W', 'X', 'Y', 'G', 'Z', 'A', 'S', 'J', 'K', 'F', 'E', 'P', 'O', 'U', 'N', 'H', 'M', 'Q', 'C', 'D', 'I'};
+//char STATIC_ROTOR[26]           = {'I', 'D', 'C', 'Q', 'M', 'H', 'N', 'U', 'O', 'P', 'E', 'F', 'K', 'J', 'S', 'A', 'Z', 'G', 'Y', 'X', 'W', 'R', 'V', 'B', 'L', 'T'}; // reverse
 const char ROTOR_I_BASE[26]     = {'L', 'B', 'V', 'O', 'J', 'U', 'Z', 'C', 'G', 'P', 'S', 'Y', 'D', 'T', 'E', 'I', 'H', 'X', 'M', 'A', 'W', 'N', 'Q', 'F', 'R', 'K'};
+//constexpr char ROTOR_I_BASE[26]     = {'K', 'R', 'F', 'Q', 'N', 'W', 'A', 'M', 'X', 'H', 'I', 'E', 'T', 'D', 'Y', 'S', 'P', 'G', 'C', 'Z', 'U', 'J', 'O', 'V', 'B', 'L'}; // reverse
 const char ROTOR_II_BASE[26]    = {'G', 'L', 'R', 'P', 'K', 'W', 'E', 'F', 'S', 'O', 'J', 'Q', 'A', 'U', 'D', 'V', 'I', 'C', 'M', 'Y', 'N', 'X', 'T', 'Z', 'H', 'B'};
+//constexpr char ROTOR_II_BASE[26]    = {'B', 'H', 'Z', 'T', 'X', 'N', 'Y', 'M', 'C', 'I', 'V', 'D', 'U', 'A', 'Q', 'J', 'O', 'S', 'F', 'E', 'W', 'K', 'P', 'R', 'L', 'G'}; // reverse
 char REFLECTOR[26]              = {'Z', 'R', 'J', 'S', 'M', 'I', 'P', 'W', 'F', 'C', 'L', 'K', 'E', 'V', 'U', 'G', 'Y', 'B', 'D', 'X', 'O', 'N', 'H', 'T', 'Q', 'A'};
 
-char getLetterFromRotor(int index, char* r) {
+char getLetterFromRotor(const int index, const char* r) {
     return r[index];
 }
 
-int getIndexFromRotor(char letter, char* r) {
+int getIndexFromRotor(const char letter, const char* r) {
     for (int i = 0; i < 26; i++) {
         if (r[i] == letter) {
             return i;
@@ -35,13 +38,13 @@ typedef struct {
     int index;
 } Letter;
 
-Letter processRotor(Letter l, char* r) {
+Letter processRotor(Letter l, const char* r) {
     l.c = getLetterFromRotor(l.index, r);
     l.index = getIndexFromRotor(l.c, ROMAN);
     assert(l.index != -1);
     return l;
 }
-Letter processReverseRotor(Letter l, char* r) {
+Letter processReverseRotor(Letter l, const char* r) {
     l.index = getIndexFromRotor(l.c, r);
     assert(l.index != -1);
     l.c = getLetterFromRotor(l.index, ROMAN);
@@ -63,7 +66,7 @@ std::string enigma(const std::string& input) {
     for (char c : input) {
         c = toupper(c);
 
-        int index = getIndexFromRotor(c, ROMAN);
+        const int index = getIndexFromRotor(c, ROMAN);
         assert(index != -1);
 
         Letter l = {c, index};
@@ -102,13 +105,13 @@ std::string enigma(const std::string& input) {
 }
 
 int main() {
-    std::string originalMessage = "HELLOWORLD";
+    const std::string originalMessage = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    std::string encryptedMessage = enigma(originalMessage);
+    const std::string encryptedMessage = enigma(originalMessage);
     std::cout << "Encrypted message: " << encryptedMessage << std::endl;
     std::cout << std::endl;
 
-    std::string decryptedMessage = enigma(encryptedMessage);
+    const std::string decryptedMessage = enigma(encryptedMessage);
     std::cout << "Decrypted message: " << decryptedMessage << std::endl;
 
     return 0;
